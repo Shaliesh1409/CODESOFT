9522dc341847404d8c81ec900805992f4ee59641
Shaliesh1409 <137253483+Shaliesh1409@users.noreply.github.com>
2024-01-14 23:06:26 +0530

    project 3 uploaded

--- project3/script.js
+++ project3/script.js
@@ -1,40 +1,9 @@
-console.log("script running");
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("script running");
+// script.js
 
-  var crossElement = document.querySelector(".cross");
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('dark-mode-toggle-btn');
 
-  if (crossElement) {
-    crossElement.style.display = "none";
-  } else {
-    console.error('Element with class "cross" not found.');
-  }
-
-  document.querySelector(".ham").addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("sidebarGo");
-
-    if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
-      document.querySelector(".ham").style.display = "inline";
-      document.querySelector(".cross").style.display = "none";
-    } else {
-      document.querySelector(".ham").style.display = "none";
-      setTimeout(() => {
-        document.querySelector(".cross").style.display = "inline";
-      }, 300);
-    }
-  });
-
-  document.querySelector(".cross").addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("sidebarGo");
-
-    if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
-      document.querySelector(".ham").style.display = "inline";
-      document.querySelector(".cross").style.display = "none";
-    } else {
-      document.querySelector(".ham").style.display = "none";
-      setTimeout(() => {
-        document.querySelector(".cross").style.display = "inline";
-      }, 200);
-    }
-  });
+    darkModeToggle.addEventListener('change', function () {
+        document.body.classList.toggle('dark-mode', darkModeToggle.checked);
+    });
 });
